cdf8329fe38ac21fc9d5f862e315d4c44b1ac074
Zak Laughton <zlaughton@gmail.com>
2018-10-01 22:35:47 -0500

    feat: add sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './Map';
+import Sidebar from './Sidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Sidebar />
         <Map />
       </div>
     );
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <div class='sidebar'>
+    <h1>SIDEBAR</h1>
+  </div>
+);
+
+export default Sidebar;
