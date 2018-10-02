a69574ca465736cdedca2a935dcbcc6a4c3ba8f4
Zak Laughton <zlaughton@gmail.com>
2018-10-01 20:47:13 -0500

    feat: put map on page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* global google */
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -13,8 +15,66 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ContactBody />
       </div>
     );
+  }
+}
+
+class ContactBody extends React.Component {
+  getGoogleMaps() {
+    // If we haven't already defined the promise, define it
+    if (!this.googleMapsPromise) {
+      this.googleMapsPromise = new Promise((resolve) => {
+        // Add a global handler for when the API finishes loading
+        window.resolveGoogleMapsPromise = () => {
+          // Resolve the promise
+          resolve(google);
+
+          // Tidy up
+          delete window.resolveGoogleMapsPromise;
+        };
+
+        // Load the Google Maps API
+        const script = document.createElement("script");
+        const API = 'AIzaSyDhlOiZW9I8f9t6V5KzLbBbi1LKc-N7C38';
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
+        script.async = true;
+        document.body.appendChild(script);
+      });
+    }
+
+    // Return a promise for the Google Maps API
+    return this.googleMapsPromise;
+  }
+
+  componentWillMount() {
+    // Start Google Maps API loading since we know we'll soon need it
+    this.getGoogleMaps();
+  }
+
+  componentDidMount() {
+    // Once the Google Maps API has finished loading, initialize the map
+    this.getGoogleMaps().then((google) => {
+      const uluru = {lat: -25.363, lng: 131.044};
+      const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+      });
+      const marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <div id="map" style={{width: 400, height: 300}}></div>
+      </div>
+    )
   }
 }
 
