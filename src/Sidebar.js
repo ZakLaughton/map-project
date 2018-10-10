bf1533b60f2008129624ab94773018df34016e84
Zak Laughton <zlaughton@gmail.com>
2018-10-10 07:39:23 -0500

    feat: add clickability to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Sidebar = ({ updateSearchResults, restaurants, isShowingRestaurant }) => (
+const Sidebar = ({ updateSearchResults, restaurants, isShowingRestaurant, handleClick }) => (
   <div className='sidebar'>
     <h1>SIDEBAR</h1>
     <input type="text" id="restaurantFilter" onInput={(event) => updateSearchResults(event.target.value)}
@@ -9,8 +9,8 @@ const Sidebar = ({ updateSearchResults, restaurants, isShowingRestaurant }) => (
       { restaurants.map((restaurant, index) => (
         <li
           key={ index }
-          className={ (isShowingRestaurant(restaurant.title) ? (restaurant.isSelected ? 'selected' : '') : 'inactive')
-                      }>
+          className={ (isShowingRestaurant(restaurant.title) ? (restaurant.isSelected ? 'selected' : '') : 'inactive')}
+          onClick={() => handleClick(restaurant)}>
           {restaurant.title}
         </li>
       ))}
