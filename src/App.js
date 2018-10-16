46010bb331004464a752290c7dc1b723aee97111
Zak Laughton <zlaughton@gmail.com>
2018-10-15 20:57:29 -0500

    feat: add function to grab photo ID

--- src/App.js
+++ src/App.js
@@ -8,30 +8,63 @@ class App extends Component {
     restaurants: [
       {title: 'Baked \'n Sconed',
        location: {lat: 30.5427284, lng: -97.55608029999999},
+       locationID: '4c0ac9687e3fc9281f44f482',
+       photoID: '',
        isShowing: true,
        isSelected: false},
       {title: 'Rio Grande Tex Mex',
        location: {lat: 30.5431313, lng: -97.55684219999999},
+       locationID: '4c0ac9687e3fc9281f44f482',
+       photoID: '',
        isShowing: true,
        isSelected: false},
       {title: 'The Downtown Hall of Fame',
        location: {lat: 30.5442963, lng: -97.54738239999999},
+       locationID: '4c0ac9687e3fc9281f44f482',
+       photoID: '',
        isShowing: true,
        isSelected: false},
       {title: 'Hit the Spot Cafe',
        location: {lat: 30.5422514, lng: -97.5454403},
+       locationID: '4c0ac9687e3fc9281f44f482',
+       photoID: '',
        isShowing: true,
        isSelected: false},
       {title: 'Snuffy\'s',
        location: {lat: 30.5431014, lng: -97.54771359999999},
+       locationID: '4c0ac9687e3fc9281f44f482',
+       photoID: '',
        isShowing: true,
        isSelected: false},
       {title: 'Texan Cafe',
        location: {lat: 30.5443816, lng: -97.54751309999999},
+       locationID: '4c0ac9687e3fc9281f44f482',
+       photoID: '',
        isShowing: true,
        isSelected: false}
     ],
     showingRestaurants: []
+  }
+
+  getRestaurantPhoto(restaurantID) {
+    const clientID = 'SO0U3NLYRUSUMZ2IOLFIEWD2ZUNMIRCYJCC4C4ZYJRHMVWXV'
+    const clientSecret = 'IL1XMEUV2KPD2134P50XWQXNS4H34CN4FWROMZ2TBFRZQDXJ'
+    const APIURL = `https://api.foursquare.com/v2/venues/${restaurantID}?client_id=${clientID}&client_secret=${clientSecret}&v=20180323`
+    fetch(APIURL)
+      .then((response) => response.json())
+      .then(data => {
+        const venue = data.response.venue;
+        const photoID = venue.bestPhoto.id;
+        console.log(photoID)
+      }).catch(function() {
+        console.log('FAIL!', data => data);
+    });
+  }
+
+  componentDidMount() {
+    this.state.restaurants.map((restaurant) => {
+      this.getRestaurantPhoto(restaurant.locationID)
+    })
   }
 
   updateSearchResults = (query) => {
