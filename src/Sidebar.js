e08162afe0abbb19eaf841b414177041def5f372
Zak Laughton <zlaughton@gmail.com>
2018-10-23 06:43:58 -0500

    feat: make sidebar more accessible

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,11 +5,13 @@ const Sidebar = ({ updateSearchResults, restaurants, handleClick }) => (
     <h1>Hutto Restaurants</h1>
     <input type="text" id="restaurantFilter" onInput={(event) => updateSearchResults(event.target.value)}
  placeholder="Filter restaurants..." title="Type in a name"></input>
-    <ul>
+    <ul role='listbox'>
       { restaurants.map((restaurant, index) => (
         <li
           key={ index }
           className={ (restaurant.isShowing) ? (restaurant.isSelected ? 'selected' : '') : 'inactive'}
+          role='button'
+          tabindex='0'
           onClick={() => handleClick(restaurant)}>
           {restaurant.title}
         </li>
